7c7e3c86a8ae7f74bf1ee66bd6ab93dd78bc8e08
Armine Palanjyan <a.p.t2@mail.ru>
2026-01-20 21:53:42 +0400

    imported assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,10 +1,7 @@
+import { strict as assert } from 'node:assert'
 import capitalize from '../src/capitalize.js'
 
-if(capitalize('hello') !== 'Hello') {
-    throw new Error ('Функция не работает')
-}
-if(capitalize('') !== '') {
-    throw new Error ('Функция не работает')
-}
+assert.equal(capitalize('hello'), 'Hello')
+assert.equal(capitalize(''), '')
 
 console.log('Все тесты пройдены')
